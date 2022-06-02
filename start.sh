eee3ee7b23e5f7469b8306c5dce0a53feb34c303
MLTB-21 <105631755+MLTB-21@users.noreply.github.com>
2022-06-02 16:23:15 +0600

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,1 +1,5 @@
+docker container prune --force || true
+docker build . --rm --force-rm --compress --no-cache=true --pull --file Dockerfile -t mirror-bot
+docker image ls
+docker run --privileged --rm -i mirror-bot
 python3 update.py && python3 -m bot
